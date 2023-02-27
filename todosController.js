985bf5866553ef1c614fc72d55bedd75a6c3cd7f
ahmedalnimr <122454313+ahmedalnimr@users.noreply.github.com>
2023-02-27 21:42:48 +0000

    add file

---
+++ todosController.js
@@ -0,0 +1,71 @@
+// this will be where I store all of my function for handling todo opreations
+
+const createError = require('http-errors')
+
+let todos = []
+let idNo = 0
+
+exports.index = function(req, res) {
+    res.send(todos)
+}
+
+exports.create = function(req, res, next) {
+
+    if (!req.body.name) {
+        return (next(createError(400, "Name is required")))
+    }
+
+    todos.push({
+        id:idNo,
+        name: req.body.name,
+    })
+    idNo++
+
+    res.send({
+        result: true
+    })
+}
+
+exports.show = function(req, res, next) {
+    const todoItem = todos.find(todo => todo.id == req.params.id)
+    if (!todoItem) {
+        return (next(createError(404, "Todo not found.")))
+    }
+
+    res.send(todoItem)
+}
+
+exports.delete = function(req, res, next) {
+    const todoItem = todos.find(todo => todo.id == req.params.id)
+    if (!todoItem) {
+        return (next(createError(404, "Todo not found.")))
+    }
+
+    todos = todos.filter(todo => todo.id != req.params.id)
+    res.send({
+        result: true
+    })
+
+}
+
+exports.update = function(req, res, next) {
+    if (!req.body.name) {
+        return (next(createError(404, "name is required.")))
+    }
+    const todoItem = todos.find(todo => todo.id == req.params.id)
+    if (!todoItem) {
+        return (next(createError(404, "Todo not found.")))
+    }
+
+    todos = todos.map(todo => {
+        if (todo.id == req.params.id) {
+            todo.name = req.body.name
+        }
+        return todo
+    })
+
+    res.send({
+        result: true
+    })
+
+}
